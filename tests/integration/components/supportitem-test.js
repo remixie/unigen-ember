da7cc5d0bea820d0fb41e5bdf014cd371699b7e3
Francisco Salinas <remixielive@live.com>
2022-05-23 22:22:00 -0400

    Everyday we stray further from God.

--- tests/integration/components/supportitem-test.js
+++ tests/integration/components/supportitem-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | supportitem', function (hooks) {
+module('Integration | Component | support-items', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Supportitem />`);
+    await render(hbs`<SupportItems />`);
 
     assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
-      <SupportItem>
+      <SupportItems>
         template block text
-      </SupportItem>
+      </SupportItems>
     `);
 
     assert.dom(this.element).hasText('template block text');
