ff1f43761c585cb7eecf7d08732cfdb803447893
Francisco Salinas <remixielive@live.com>
2022-05-23 20:24:44 -0400

    Add improvements

---
+++ app/components/support-items.js
@@ -0,0 +1,33 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+
+export default class SupportItemComponent extends Component {
+  @tracked supportlist = [
+    {
+      heading_title: "Unigen Tutorials",
+      links: [
+        {
+          title: 'Test Plan Generation',
+          url: 'https://example.com/'
+        },
+        {
+          title: 'SQL Generation',
+          url: 'https://wikipedia.org/'
+        },
+      ]
+    },
+    {
+      heading_title: "Visual Aids",
+      links: [
+        {
+          title: 'Substrates & Barcodes',
+          url: 'https://example.com/'
+        },
+        {
+          title: 'Cool things',
+          url: 'https://wikipedia.org/'
+        },
+      ]
+    }
+  ];
+}
